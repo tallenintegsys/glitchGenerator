410f659d5befe5f3d14e0e631ca990f85e74b360
tallenintegsys <tallen@integsys.biz>
2023-05-20 21:39:05 -0400

    working out the "UI"

---
+++ ui.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "                           ___________          "
+echo -e "                          |   pulse   |         "
+echo -e "      delay  width        |   width   |         "
+echo -e "      \e[100m          \e[m ns       | \e[100m      \e[m ns |         "
+echo -e "__________________________|           |_________"
+echo 
+echo -e "tab betwixt fields     space to manually trigger"
